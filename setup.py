5da5a5da06ef9a9a863560635f740b847f73ec1d
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2021-02-06 15:12:18 +0000

    New version for reference.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='RuleFit',
-      version='0.3',
+      version='0.3.1',
       description='RuleFit algorithm',
       author='Christoph Molnar',
       author_email='christoph.molnar@gmail.com',
